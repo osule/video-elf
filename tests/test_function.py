ea412ea4dce69c2b056515078733cc665fb9098c
Oluwafemi Sule <oluwafemisule@outlook.com>
2018-01-29 13:26:25 +0100

    test media info extractor

---
+++ tests/test_function.py
@@ -0,0 +1,23 @@
+import json
+from mock import Mock
+
+from video_elf import extract_media_urls
+
+
+def test_extract_media_url():
+    event = Mock(
+        body=json.dumps({
+            'url': 'https://youtu.be/a8MnOv0uxkY'
+        })
+    )
+    response = Mock()
+    context = Mock(Response=response)
+    media_info = extract_media_urls(context, event)
+
+    args, kwargs = response.call_args
+
+    assert response.called is True
+    assert kwargs['content_type'] == 'application/json'
+    assert kwargs['status_code'] == 200
+    body = json.loads(kwargs['body'])
+    assert 'id' in body
