733b5de15948a488022a20a59a82b3dec90c8e39
Oluwafemi Sule <oluwafemisule@outlook.com>
2018-01-29 13:27:50 +0100

    extract media urls from links

---
+++ video_elf.py
@@ -0,0 +1,22 @@
+import json
+
+from youtube_dl import YoutubeDL
+
+youtube_dl = YoutubeDL()
+
+
+def extract_media_urls(context, event):
+    context.logger.info('Extracting media information from Youtube url')
+
+    body = json.loads(event.body)
+    url = body['url']
+
+    media_info = youtube_dl.extract_info(url, download=False)
+    body = json.dumps(media_info)
+
+    return context.Response(
+        body=body,
+        headers={},
+        content_type='application/json',
+        status_code=200
+    )
